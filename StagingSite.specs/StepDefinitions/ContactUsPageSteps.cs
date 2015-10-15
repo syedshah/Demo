e95e7c9d0718cf64c47260a732d72fda8dd79a44
syedshah <syed.naeem@gmail.com>
2015-10-15 17:36:33 +0100

    amended

--- StagingSite.specs/StepDefinitions/ContactUsPageSteps.cs
+++ StagingSite.specs/StepDefinitions/ContactUsPageSteps.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
 
@@ -19,13 +20,17 @@ namespace StagingSite.specs
         [When]
         public void When_I_click_the_send_button()
         {
-            ScenarioContext.Current.Pending();
+            var p = WebBrowser.Current.Page<ContactUsPage>();
+            p.ClickSendButton();
         }
         
         [Then]
         public void Then_I_should_be_able_to_contact_QAWorks_with_the_following_information(Table table)
         {
-            ScenarioContext.Current.Pending();
+            var page = WebBrowser.Current.Page<ContactUsPage>();
+            Assert..AreEqual(page.Name,  table.Rows[1]["value"]);
+            Assert..AreEqual(page.Email,  table.Rows[2]["value"]);
+            Assert..AreEqual(page.Message,  table.Rows[3]["value"]);
         }
     }
 }
